4d9ec1ca98670cf214a9a85cd0158f9a133545d5
Niek van der Maas <mail@niekvandermaas.nl>
2022-06-01 15:17:33 +0200

    Fix download link

--- link.js
+++ link.js
@@ -1,10 +1,12 @@
 const { chromium } = require('playwright-chromium');
 
 (async () => {
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto('https://www.microsoft.com/en-us/software-download/vlacademicwindows10iso');
-  await page.selectOption('select#product-languages', { label: 'English' } );
+  await page.goto('https://www.microsoft.com/en-us/software-download/windows10ISO');
+  await page.selectOption('select#product-edition', { label: 'Windows 10 (multi-edition ISO)' } );
+  await (await page.$('#submit-product-edition')).click();
+  await page.selectOption('select#product-languages', { label: 'English International' } );
   await (await page.$('#submit-sku')).click();
   const button = await page.waitForSelector('#card-info-content a');
   const url = await button.getAttribute('href');
